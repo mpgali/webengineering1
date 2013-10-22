485c06063f622f90781881e49443d5a23968dade
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 00:13:16 +0200

    49

--- menu.php
+++ menu.php
@@ -2,5 +2,5 @@
     <li><a href="index.php">Home</a></li>
     <li></li>
     <li></li>
-    <li>I<a href="impressum.php">Impressum</a></li>
+    <li><a href="impressum.php">Impressum</a></li>
 </ul>
