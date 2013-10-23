c4014bf902a82da8a79341978531608802967f25
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 10:28:17 +0200

    75

--- referenzen.php
+++ referenzen.php
@@ -1,1 +1,8 @@
 <h1>Referenzen</h1>
+<article>
+<h1>Schweden-Seite</h1>
+<p>
+<b>Projektbeschreibung<b></br>
+Ziel des Projektes ist es, die Länder eines Kontinentes übers Internet zu präsentieren. Die Darstellung eines Landes soll mit den Mitteln von Multimedia bzw. WWW erfolgen. Neben einer Beschreibung des Landes in Textform sollen Bilder und Filme oder Java-Applets einen Eindruck von dem Land vermitteln. Die multimedialen Inhalte sollen dabei auf landestypische Eigenarten eingehen, mit denen sich ein Land identifizieren lässt.
+</p>
+</article>
