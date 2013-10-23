7cc8d405c8f2472ee880a7ce47b6e06e0e759081
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 21:05:41 +0200

    125

--- impressum.php
+++ impressum.php
@@ -11,5 +11,5 @@ $hintergund_farbe  = imagecolorallocate ($bild, 244, 244, 244);
 $text_farbe        = imagecolorallocate ($bild, 0, 0, 0);
  
 imagestring ($bild, $textnr, 0, 0, $email, $text_farbe);
-imagepng ($bild;
+imagepng ($bild);
 ?>
