87e585c27e64baa11366054e37debf6fc2421409
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 21:04:10 +0200

    124

--- impressum.php
+++ impressum.php
@@ -5,11 +5,11 @@ $textbreite = imagefontwidth($textnr) * strlen($email);
 $texthoehe  = imagefontheight($textnr);
  
 header ("Content-type: image/png");
-$bild2 = imagecreate ($textbreite , $texthoehe);
+$bild = imagecreate ($textbreite , $texthoehe);
  
 $hintergund_farbe  = imagecolorallocate ($bild, 244, 244, 244);
 $text_farbe        = imagecolorallocate ($bild, 0, 0, 0);
  
-imagestring ($bild2, $textnr, 0, 0, $email, $text_farbe);
-imagepng ($bild2;
+imagestring ($bild, $textnr, 0, 0, $email, $text_farbe);
+imagepng ($bild;
 ?>
