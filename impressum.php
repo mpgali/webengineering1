378353566da88e7dac813c945566b6c2761ecbea
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 21:11:48 +0200

    126

--- impressum.php
+++ impressum.php
@@ -1,6 +1,6 @@
 <?php
 $textnr = 4;
-header ("Content-type: image/png");
+header ("Content-type: image/gif");
 $bild = imagecreate (400 , 50);
 imagecolorallocate ($bild, 0, 0, 0);
 $text_farbe = ImageColorAllocate ($bild, 255, 255, 0);
