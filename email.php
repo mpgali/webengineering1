aedce5ffc742dcdcf10306f5c03e56f97de1d1ce
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 20:41:41 +0200

    114

--- email.php
+++ email.php
@@ -5,12 +5,12 @@ $textbreite = imagefontwidth($textnr) * strlen($email);
 $texthoehe  = imagefontheight($textnr);
  
 //header ("Content-type: image/png");
-$bild = imagecreate ($textbreite , $texthoehe);
+$bild2 = imagecreate ($textbreite , $texthoehe);
  
 $hintergund_farbe  = imagecolorallocate ($bild, 244, 244, 244);
 $text_farbe        = imagecolorallocate ($bild, 0, 0, 0);
  
-imagestring ($bild, $textnr, 0, 0, $email, $text_farbe);
-imagepng ($bild);
+imagestring ($bild2, $textnr, 0, 0, $email, $text_farbe);
+imagepng ($bild2;
 ?>
 
--- name.php
+++ name.php
@@ -5,12 +5,12 @@ $textbreite = imagefontwidth($textnr) * strlen($email);
 $texthoehe  = imagefontheight($textnr);
  
 //header ("Content-type: image/png");
-$bild = imagecreate ($textbreite , $texthoehe);
+$bild1 = imagecreate ($textbreite , $texthoehe);
  
 $hintergund_farbe  = imagecolorallocate ($bild, 244, 244, 244);
 $text_farbe        = imagecolorallocate ($bild, 0, 0, 0);
  
-imagestring ($bild, $textnr, 0, 0, $email, $text_farbe);
-imagepng ($bild);
+imagestring ($bild1, $textnr, 0, 0, $email, $text_farbe);
+imagepng ($bild1);
 ?>
 
