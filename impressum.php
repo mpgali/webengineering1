9350024c84e8c74e1e35904d9baa288e53583c54
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 21:13:05 +0200

    127

--- impressum.php
+++ impressum.php
@@ -6,6 +6,6 @@ imagecolorallocate ($bild, 0, 0, 0);
 $text_farbe = ImageColorAllocate ($bild, 255, 255, 0);
  
 ImageString ($bild, $textnr, 0, 0, "Hallo Welt", $text_farbe);
-ImagePNG ($bild);
+ImageGIF ($bild);
 ?>
 
