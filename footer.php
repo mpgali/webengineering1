b4afc654c2411aa7a1343a2285fa4451d6ee0dcf
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 22:16:49 +0200

    146

--- footer.php
+++ footer.php
@@ -4,4 +4,5 @@ Gefördert von:</br></br>
 <a href="http://www.thm.de" title="Technische Hochschule Mittelhessen"><img src="image/THM_Logo_klein.png"></a>&nbsp;
 <a href="http://www.cloudcontrol.com" title="Clous App Platform"><img src="image/cloudcontrol.png"></a>&nbsp;
 <a href="https://iversity.org" title="Education.Free.Online"><img src="image/iversity.png"></a>&nbsp;
+<a href="https://github.com" title="GitHub-Build Software better,together."><img src="image/github.png"></a>&nbsp;
 </p>
