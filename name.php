db28ce00959e004e160476d17072fe649ad0cf2d
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 19:41:32 +0200

    94

---
+++ name.php
@@ -0,0 +1,16 @@
+<?php
+$email = "Frank Wollenweber";
+$textnr = 4;
+$textbreite = imagefontwidth($textnr) * strlen($email);
+$texthoehe  = imagefontheight($textnr);
+ 
+header ("Content-type: image/png");
+$bild = imagecreate ($textbreite , $texthoehe);
+ 
+$hintergund_farbe  = imagecolorallocate ($bild, 244, 244, 244);
+$text_farbe        = imagecolorallocate ($bild, 0, 0, 0);
+ 
+imagestring ($bild, $textnr, 0, 0, $email, $text_farbe);
+imagepng ($bild);
+?>
+
