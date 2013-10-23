7ee8758c604656fc5628cb0dac14819801b30ec8
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 20:20:25 +0200

    102

--- sites.php
+++ sites.php
@@ -6,12 +6,7 @@ switch ($section)
 case "impressum":
       include("impressum.php");
   break;
-  case "email":
-      include("email.php");
-  break;
-  case "name":
-      include("name.php");
-  break;
+
   case "referenzen":
       include("referenzen.php");
   break;
