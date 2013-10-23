e53b27bec8f61f25f551b13ea1afd75f9dbe3cb7
Frank Wollenweber <dersenaor@gmx.net>
2013-10-23 19:35:51 +0200

    92

--- test.php
+++ test.php
@@ -1,20 +1,16 @@
 <?php
-
-    function emailPic($email)
-    {
-        header("Content-type:image/png");
-
-        $image =        imagecreate (150,16);
-        $background =   imagecolorallocate($image,255,255,255);
-        $color =        imagecolorallocate($image,102,102,102);
-        $font =         "fonts/arial.ttf";
-
-        imageTTFtext($image,9,0,5,12,$color,$font,$email);
-        imagegif($image,"email.png");
-    }
-
-    $email = "system23@hotmail.de";
-    
-   emailPic($email);
-    echo '[img]email.png[/img]';
+$email = "hier kommt Ihre E-Mail-Adresse";
+$textnr = 4;
+$textbreite = imagefontwidth($textnr) * strlen($email);
+$texthoehe  = imagefontheight($textnr);
+ 
+header ("Content-type: image/png");
+$bild = imagecreate ($textbreite , $texthoehe);
+ 
+$hintergund_farbe  = imagecolorallocate ($bild, 222, 222, 222);
+$text_farbe        = imagecolorallocate ($bild, 0, 0, 0);
+ 
+imagestring ($bild, $textnr, 0, 0, $email, $text_farbe);
+imagepng ($bild);
 ?>
+
