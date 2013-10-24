2ecb2a99e44a1afde94119a9484d1f88a30c1c1a
Frank Wollenweber <dersenaor@gmx.net>
2013-10-24 03:20:04 +0200

    2

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
 <p>
 &copy; 2013 Frank Wollenweber All Rights reserved </br>
 Gefördert von:</br></br>
-<a href="http://www.thm.de" title="Technische Hochschule Mittelhessen"><img src="image/THM_Logo_klein.png"></a>&nbsp;
-<a href="http://www.cloudcontrol.com" title="Clous App Platform"><img src="image/cloudcontrol.png"></a>&nbsp;
-<a href="https://iversity.org" title="Education.Free.Online"><img src="image/iversity.png"></a>&nbsp;
-<a href="https://github.com" title="GitHub-Build Software better,together."><img src="image/github.png"></a>&nbsp;
 </p>
+<?php
+include("sponsors.php");
+?>
