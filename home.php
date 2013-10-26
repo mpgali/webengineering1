c6f20c952196041fc017ceb9ad2d1c345a459342
Frank Wollenweber <dersenaor@gmx.net>
2013-10-26 19:08:17 +0200

    1

--- home.php
+++ home.php
@@ -1,4 +1,34 @@
 <article>
+<h1>Git CLI Cheats</h1>
+<p>
+Zur Konfiguration von Git die folgenden Befehle eingeben:</br>
+</br>
+- git config -global user.name "Vorname Nachname"</br>
+- git config -global user.email "vorname.nachname@mail.de"</br>
+- git config -global color.ui auto</br>
+</br>
+- <strong>git status</strong> fragt den Status des Git-Repositorys ab</br>
+</br>
+Die rote Farbe deutet darauf hin, dass Änderungen an der Datei noch nicht
+überwacht werden.</br>
+Die grüne Farbe hingegen zeigt an, dass die Datei von Git überwacht wird.</br>
+</br>
+- <strong>git init</strong> kann das Verzeichnis als Git Repository initiiert werden</br>
+- <strong>git add "Dateiname"</strong> Dateien zur Überwachung in das Git-Repository hinzufügen</br>
+- <strong>git add *</strong> können alternativ mehrere Dateien zur Überwachung hinzugefügt werden</br>
+- <strong>git commit -m "Benutzerdefinierte Nachricht"</strong> die Ännderungen bestätigen</br>
+- <strong>git reset HEAD "Dateiname"</strong> kann die Datei wieder aus der Liste der überwachten Dateien herausgenommen werden</br>
+- <strong>git checkout -- "Dateiname"</strong> können Änderungen an einer Datei wieder rückgängig gemacht werden</br>
+- <strong>git remote add origin [Github-Repository] </strong>kann das lokale Repository mit dem Remote-Repository verbunden werden</br>
+- <strong>git push -u origin master</strong> werden die lokalen Änderungen auf den Remote-Repository gespiegelt</br>
+</br>
+Zum Üben der Kommandos <a href="http://try.github.io/levels/1/challenges/1" title="try Git">http://try.github.io</a></br>
+Literatur zum Vertiefen Git-eBook: <a href="http://www.git-scm.com/book/de" title="Git-Handbuch">http://www.git-scm.com/book/de</a>
+</br>
+
+
+</article>
+<article>
 <h1>Hallo Welt</h1>
 <p>
 Eine neue Webseite erblickt die Welt.Programmiert mit PHP,HTML5 und CSS3.</br>
@@ -7,7 +37,7 @@ Dies ist Teil des MOOC " WebEngineering Teil1" der THM - Technische Hochschule M
 </p>
 </article>
 <article>
-<h1>CLI Cheats</h1>
+<h1>CloudControl CLI Cheats</h1>
 <p>Kommandos zum Upload in die Cloud:</br>
 - git add *</br>
 - git commit -m"Commit 1"</br>
