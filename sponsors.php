6807de3322ff96675fdd14b6650e3bcebeb30b8c
Frank Wollenweber <dersenaor@gmx.net>
2013-11-20 22:44:06 +0100

    2

--- sponsors.php
+++ sponsors.php
@@ -3,5 +3,6 @@
 <a href="http://www.cloudcontrol.com" title="Cloud App Platform"><img src="image/cloudcontrol.png"></a>&nbsp;
 <a href="https://iversity.org" title="Education.Free.Online"><img src="image/iversity.png"></a>&nbsp;
 <a href="https://github.com" title="GitHub-Build Software better,together."><img src="image/github.png"></a>&nbsp;
-<a href="https://www.uberspace.de" title="Hosting on Asteroids"><img src="image/uberspace.png"></a>&nbsp;
+<a href="http://www.uberspace.de" title="Hosting on Asteroids"><img src="image/uberspace.png"></a>&nbsp;
+<a href="http://www.coecademy.com" title="Codecademy,teaching the to code"><img src="image/codecademy.png"></a>&nbsp;
 </p>
