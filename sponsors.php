25d590be30a4fa6668582248ad6be50071834f26
Frank Wollenweber <dersenaor@gmx.net>
2013-12-04 20:32:33 +0100

    2

--- sponsors.php
+++ sponsors.php
@@ -1,8 +1,8 @@
 <p>
-<a href="http://www.thm.de" title="Technische Hochschule Mittelhessen"><img src="image/THM_Logo_klein.png"></a>&nbsp;
-<a href="http://www.cloudcontrol.com" title="Cloud App Platform"><img src="image/cloudcontrol.png"></a>&nbsp;
-<a href="https://iversity.org" title="Education.Free.Online"><img src="image/iversity.png"></a>&nbsp;
-<a href="https://github.com" title="GitHub-Build Software better,together."><img src="image/github.png"></a>&nbsp;
-<a href="http://www.uberspace.de" title="Hosting on Asteroids"><img src="image/uberspace.png"></a>&nbsp;
-<a href="http://www.coecademy.com" title="Codecademy,teaching the world to code"><img src="image/codecademy.png"></a>&nbsp;
+<a href="http://www.thm.de" title="Technische Hochschule Mittelhessen"><img src="image/THM_Logo_klein.png" alt="technische Hochschule Mittelhessen"></a>&nbsp;
+<a href="http://www.cloudcontrol.com" title="Cloud App Platform"><img src="image/cloudcontrol.png" alt="www.cloudcontrol.com"></a>&nbsp;
+<a href="https://iversity.org" title="Education.Free.Online"><img src="image/iversity.png" alt="www.iversity.org"></a>&nbsp;
+<a href="https://github.com" title="GitHub-Build Software better,together."><img src="image/github.png" alt="www.github.com"></a>&nbsp;
+<a href="http://www.uberspace.de" title="Hosting on Asteroids"><img src="image/uberspace.png" alt="www.uberspace.de"></a>&nbsp;
+<a href="http://www.coecademy.com" title="Codecademy,teaching the world to code"><img src="image/codecademy.png" alt="www.codecademy.com"></a>&nbsp;
 </p>
