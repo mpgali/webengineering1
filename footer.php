4a0413cb54c292239429f97c837d2483e01c0441
Frank Wollenweber <dersenaor@gmx.net>
2013-12-04 20:29:31 +0100

    2

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <p>
-&copy; 2013 Frank Wollenweber All Rights reserved </br>
-Gefördert von:</br></br>
+&copy; 2013 Frank Wollenweber All Rights reserved <br>
+Gefördert von:<br><br>
 </p>
 <?php
 include("sponsors.php");
